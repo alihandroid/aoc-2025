68ae13fc1dcd24dc1cbc903adae2882f17965dd0
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-12-02 00:39:15 +0300

    Solve day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -2,7 +2,9 @@ use std::cmp::PartialEq;
 
 advent_of_code::solution!(1);
 
-#[derive(Eq, PartialEq)]
+const MAX_NUM: u64 = 100;
+
+#[derive(Eq, PartialEq, Debug)]
 enum Direction {
     Left,
     Right,
@@ -13,10 +15,10 @@ pub fn part_one(input: &str) -> Option<u64> {
     let mut zero_count = 0;
     for (direction, number) in parse(input) {
         position = match direction {
-            Direction::Left => position - number,
-            Direction::Right => position + number,
+            Direction::Left => position - number as i64,
+            Direction::Right => position + number as i64,
         }
-        .rem_euclid(100);
+        .rem_euclid(MAX_NUM as i64);
 
         if position == 0 {
             zero_count += 1;
@@ -26,17 +28,37 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut position = 50;
+    let mut zero_count = 0;
+    for (direction, number) in parse(input) {
+        let full_turns = if position == 0 {
+            number
+        } else {
+            match direction {
+                Direction::Left => number + MAX_NUM - position as u64,
+                Direction::Right => number + position as u64,
+            }
+        } / MAX_NUM;
+
+        position = match direction {
+            Direction::Left => position - number as i64,
+            Direction::Right => position + number as i64,
+        }
+        .rem_euclid(MAX_NUM as i64);
+
+        zero_count += full_turns;
+    }
+    Some(zero_count)
 }
 
-fn parse(input: &str) -> impl Iterator<Item = (Direction, i64)> + '_ {
-    input.lines().filter(|line| !line.is_empty()).map(|line| {
+fn parse(input: &str) -> impl Iterator<Item = (Direction, u64)> + '_ {
+    input[..input.len() - 2].lines().map(|line| {
         let direction = if line.as_bytes()[0] == b'L' {
             Direction::Left
         } else {
             Direction::Right
         };
-        let number = line[1..].parse::<i64>().unwrap();
+        let number = line[1..].parse::<u64>().unwrap();
         (direction, number)
     })
 }
@@ -54,6 +76,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
