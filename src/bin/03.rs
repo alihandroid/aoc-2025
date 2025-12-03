8dd4debeb3d9db155e6feeff1b2cce4d3fca350d
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-12-03 18:40:03 +0300

    Solve day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -7,9 +7,9 @@ pub fn part_one(input: &str) -> Option<u64> {
 fn find_max_joltage(s: &str) -> u64 {
     let bytes = s.as_bytes();
     let mut tens = 0;
-    let mut tens_index= 0;
+    let mut tens_index = 0;
 
-    for (i,b) in bytes[..bytes.len() - 1].iter().enumerate() {
+    for (i, b) in bytes[..bytes.len() - 1].iter().enumerate() {
         let num = b - b'0';
         if num > tens {
             tens = num;
@@ -18,7 +18,7 @@ fn find_max_joltage(s: &str) -> u64 {
     }
 
     let mut ones = 0;
-    for b in bytes[tens_index+1..].iter() {
+    for b in bytes[tens_index + 1..].iter() {
         let num = b - b'0';
         if num > ones {
             ones = num;
@@ -29,7 +29,28 @@ fn find_max_joltage(s: &str) -> u64 {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    Some(parse(input).map(find_max_joltage_part_two).sum())
+}
+
+fn find_max_joltage_part_two(s: &str) -> u64 {
+    const OUTPUT_DIGITS: usize = 12;
+    let bytes = s.as_bytes();
+    let mut digits: [_; OUTPUT_DIGITS] = [0; OUTPUT_DIGITS];
+    let mut digit_indices: [_; OUTPUT_DIGITS] = [0; OUTPUT_DIGITS];
+
+    for d in 0..OUTPUT_DIGITS {
+        let start = if d == 0 { 0 } else { digit_indices[d - 1] + 1 };
+        let end = bytes.len() - OUTPUT_DIGITS + d + 1;
+        for (i, b) in bytes[start..end].iter().enumerate() {
+            let num = b - b'0';
+            if num > digits[d] {
+                digits[d] = num;
+                digit_indices[d] = start + i;
+            }
+        }
+    }
+
+    digits.iter().fold(0, |acc, digit| acc * 10 + *digit as u64)
 }
 
 fn parse(input: &str) -> impl Iterator<Item = &str> + '_ {
@@ -49,6 +70,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
