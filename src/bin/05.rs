e57b2e5a4948b57a7dfe6a003665f8ec7ad5f7e9
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-12-06 19:50:04 +0300

    Solve day 5 part 1

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,11 +1,49 @@
 advent_of_code::solution!(5);
 
+struct ParsedInput {
+    ranges: Vec<(u64, u64)>,
+    ids: Vec<u64>,
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
+    let ParsedInput { ranges, ids } = parse(input);
+    let result = ids.into_iter().filter(|id| is_fresh(&ranges, id)).count() as u64;
+    Some(result)
+}
+pub fn part_two(input: &str) -> Option<u64> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+fn is_fresh(ranges: &[(u64, u64)], ids: &u64) -> bool {
+    ranges
+        .iter()
+        .any(|(start, end)| (start..=end).contains(&ids))
+}
+
+fn parse(input: &str) -> ParsedInput {
+    let sections: Vec<&str> = input.split("\n\n").collect();
+
+    let [ranges_str, ids_str] = sections[..] else {
+        panic!("invalid input");
+    };
+
+    let ranges = ranges_str
+        .lines()
+        .map(|line| {
+            let parts: Vec<u64> = line
+                .split('-')
+                .map(|s| s.parse().expect("Invalid number in range"))
+                .collect();
+            (parts[0], parts[1])
+        })
+        .collect();
+
+    let ids = ids_str
+        .lines()
+        .map(|line| line.parse().expect("Invalid ID number"))
+        .collect();
+
+    ParsedInput { ranges, ids }
 }
 
 #[cfg(test)]
@@ -15,7 +53,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
