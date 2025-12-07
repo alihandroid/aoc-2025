3a5bedbe12962d5fc3f8741d12b42c336fc20393
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-12-07 16:03:10 +0300

    Solve day 7 part 1

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,7 +1,34 @@
 advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let lines = input.lines().collect::<Vec<_>>();
+    let width = lines[0].len();
+    let mut beams = vec![false; width];
+    let start_column = lines[0].find('S').unwrap();
+    beams[start_column] = true;
+    let mut next_beams;
+    let mut split_count = 0;
+
+    for line in lines.into_iter().skip(1) {
+        next_beams = vec![false; width];
+
+        for (i, &ch) in line.as_bytes().into_iter().enumerate() {
+            if !beams[i] {
+                continue;
+            }
+            if ch == b'^' {
+                next_beams[i - 1] = true;
+                next_beams[i + 1] = true;
+                split_count += 1;
+            } else {
+                next_beams[i] = true;
+            }
+        }
+
+        beams = next_beams;
+    }
+
+    Some(split_count)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -15,7 +42,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(21));
     }
 
     #[test]
