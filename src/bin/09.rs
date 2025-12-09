4c046708bb0eb3b2713155893b0e2e83cacfa12d
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-12-09 14:14:17 +0300

    Solve day 9 part 1

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,11 +1,36 @@
 advent_of_code::solution!(9);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let points = parse(input).collect::<Vec<_>>();
+
+    (0..points.len())
+        .flat_map(|i| {
+            let points = &points;
+            (i + 1..points.len()).map(move |j| {
+                let (x1, y1) = points[i];
+                let (x2, y2) = points[j];
+                (x2.abs_diff(x1) + 1) * (y2.abs_diff(y1) + 1)
+            })
+        })
+        .max()
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
     None
+}
+
+fn parse(input: &str) -> impl Iterator<Item = (u64, u64)> {
+    input.lines().map(|line| {
+        let mut split = line.split(',');
+        let (a, b) = match (split.next(), split.next()) {
+            (Some(a), Some(b)) => (a, b),
+            _ => panic!(),
+        };
+        match (a.parse::<u64>(), b.parse::<u64>()) {
+            (Ok(a), Ok(b)) => (a, b),
+            _ => panic!(),
+        }
+    })
 }
 
 #[cfg(test)]
@@ -15,7 +40,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(50));
     }
 
     #[test]
